bda6f2fbfafbb4aa54e3b47ddf6ef634e9d8f790
fstagni <fstagni@cern.ch>
2015-04-24 12:23:43 +0200

    Added cfg update

---
+++ Jenkins/dirac-cfg-update.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+""" update local cfg
+"""
+
+from DIRAC.Core.Base import Script
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgFile] cfgFile' % Script.scriptName] ) )
+Script.parseCommandLine()
+args = Script.getPositionalArgs()
+
+import os
+
+from DIRAC.Core.Utilities.CFG import CFG
+
+localCfg = CFG()
+localConfigFile = os.path.join( '.', args[0] )
+localCfg.loadFromFile( localConfigFile )
+if not localCfg.isSection( '/LocalSite' ):
+  localCfg.createNewSection( '/LocalSite' )
+localCfg.setOption( '/LocalSite/CPUTimeLeft', 5000 )
+localCfg.setOption( '/DIRAC/Security/UseServerCertificate', False )
+localCfg.writeToFile( localConfigFile )
