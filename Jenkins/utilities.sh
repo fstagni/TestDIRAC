f5b831ecda7345eda2607076e7e525a617267903
fstagni <fstagni@cern.ch>
2015-08-12 17:48:30 +0200

    list of agents to not install

--- Jenkins/utilities.sh
+++ Jenkins/utilities.sh
@@ -528,7 +528,7 @@ diracAgents(){
 	echo '[diracAgents]'
 
 	#TODO: revise this list
-	agents=`cat agents | cut -d '.' -f 1 | grep -v LFC | grep -v MyProxy | grep -v CAUpdate | sed 's/System / /g' | sed 's/ /\//g'`
+	agents=`cat agents | cut -d '.' -f 1 | grep -v LFC | grep -v MyProxy | grep -v CAUpdate | grep -v ConfigurationSystem | grep -v FrameworkSystem | grep -v DiracSiteAgent| sed 's/System / /g' | sed 's/ /\//g'`
 	
 	for agent in $agents
 	do
