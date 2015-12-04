8e96ea1524167fd2433c3239fae9d1b805b80e24
fstagni <fstagni@cern.ch>
2015-12-04 16:37:43 +0100

    gComponentInstaller

--- Jenkins/dirac-drop-db.py
+++ Jenkins/dirac-drop-db.py
@@ -15,8 +15,7 @@ if len( args ) < 1:
   Script.showHelp()
   exit( -1 )
 
-from DIRAC.Core.Utilities import InstallTools
-
-InstallTools.getMySQLPasswords()
+from DIRAC.Core.Utilities.ComponentsInstaller import gComponentsInstaller
+gComponentsInstaller.getMySQLPasswords()
 for db in args:
-  print InstallTools.execMySQL( "DROP DATABASE IF EXISTS %s" % db )
+  print gComponentsInstaller.execMySQL( "DROP DATABASE IF EXISTS %s" % db )
